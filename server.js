8813ecd1a63614ef24d1241965de21ccc0e10fa3
Sergio Casal <sergiiiocasal@gmail.com>
2025-02-20 03:07:09 +0100

    Cambios

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
